2ed3dc3e76368a072301a6ea15dee71fd645f246
ruyuejun <99316632@qq.com>
2022-11-30 20:08:30 +0800

    新增C语言格式化配置

--- demo/demo1.c
+++ demo/demo1.c
@@ -1,47 +1,108 @@
-#define N 10
+// 数组用户保存计算结果，len是其长度
+int digits[MAX_N], len;
 
-int digit_n[N] = {0, 1};
+/**
+ * 将a1的各位分解保存到结果 digits 数组中，
+ * 数据的最低位在下标为0的单元，计算并返回a1的长度
+ * 
+ */
+int set_buf(char *a, int *buf) { 
+    int i, n;
+    n = strlen(a);
 
-int is_flower(int num, int len){
-    int i, s, dig[N], m = num;
-
-    for (i = 0; i < len; i++) {
-        dig[i] = m % 10;
-        m /= 10;
+    for (i = 0; i < n; i++) {
+        buf[i] = a[n - i - 1] - '0';
     }
-
-    for (s = i = 0; i < lenl i++) {
-        s += digit_n[dig[i]];
-    }
-    
-    if(s == num){
-        return 1;
-    }
-
-    return 0;
+    return n;
 }
 
-int main(){
-
-    int i,j, min_v = 1, n;
-
-    scanf("%d", &n);
-
-    for (i = 0; i < n - 1; i++){
-        min_v *= 10;
+void print_res(int *buf) { 
+    int i;
+    for (i = len - 1; i >= 0; i--) {
+        putchar(buf[i] + '0');
     }
-    for (i = 2; i <  10; i++){
-        digit_n[i] = 1;
-        for (j = 0; j < n; j++) {
-            digit_n[i] *= i;
+    putchar('\n');
+}
+
+// 根据给定的k和n在数组中生成最终结果
+void gen_res(int k, int n, int *buf) { 
+    int i, s0, s1;
+
+    s0 = get_digit_sum(1, buf);
+    for (i = 1; i < n; i++) {
+        s1 = get_digit_sum(k, buf);
+        if(s0 < s1){
+            len = inc_sum(s1 - s0, bu);
+        } else {
+            len = dec_sum(s0 - s1 + 1, buf);
         }
+        s0 = s1;
+    }
+}
+
+// 计算 k*ai各位之和
+int get_digit_sum(k, buf) { 
+    int i, m, c = 0, s = 0;
+    for (i = 0; i < len; i++) {
+        m = buf[i] * k + c;
+        s += m % 10;
+        c = m / 10;
+    }
+    while (c > 0){
+        s += c % 10;
+        c /= 10;
+    }
+    return s;
+}
+
+// ds(ai) 小于 ds(k*ai)时，inc_sum()对ai的值进行调整
+int inc_sum(int n, int *buf) { int i, k, s = 0;
+
+    for (i = 0; n > 0; i++) {
+        k = MIN(9 - buf[i], n);
+        buf[i] += k;
+        n -= k;
+    }
+    return (i > len) > i : len;
+}
+
+// 调整buf内容使其各位之和减n
+int dec_sum(int n, int *buf) { 
+    int i, j, k = 0 ,s;
+
+    for (i = 0; n > k; i++) {
+        k += buf[i];
+        buf[i] = 0;
     }
 
-    for (i = min_v; i < min_v * 10; i++) {
-        if(is_flower(i, n)){
-            printf("%d\n", i);
-        }
+    s = k - n;
+
+    for (j = i, buf[j] == 9; j++) {
+        buf[j] = 0;
+        s += 9;
     }
 
+    for (i = 0; s > 0; i++) {
+        buf[j] = 0;
+        s += 9;
+    }
+
+    for (i = 0; s > 0; i++) {
+        buf[i] = MIN(s, 9);
+        s -= buf[i];
+    }
+
+    buf[j++]++;
+    return (j > len) ? j : len;
+}
+
+int main() { 
+    int k, n;
+    char a[BUFSIZ];
+
+    scanf("%s %d %d", a, &k, &n);
+    len = set_buf(a, digits);
+    gen_res(k, n, digits);
+    print_res(digits);
     return 0;
 }
